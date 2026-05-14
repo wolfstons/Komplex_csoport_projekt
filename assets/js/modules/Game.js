dfaaecb88a50eab6d348846e42b8cc8856fe6884
wolfstons <paczi.balazs@gmail.com>
2026-05-14 12:46:14 +0200

    game javítva

--- assets/js/modules/Game.js
+++ assets/js/modules/Game.js
@@ -1,19 +1,41 @@
 export default class Game {
-    
+    #id;
+    #title;
+    #imagePath;
+    #shortDescription;
+    #longDescription;
+    #type;
+
     constructor({ id, title, imagePath, shortDescription, longDescription, type }) {
-        this.id = id;
-        this.title = title;
-        this.imagePath = imagePath;
-        this.shortDescription = shortDescription;
-        this.longDescription = longDescription;
-        this.type = type;
+        this.#id = id;
+        this.#title = title;
+        this.#imagePath = imagePath;
+        this.#shortDescription = shortDescription;
+        this.#longDescription = longDescription;
+        this.#type = type;
+    }
+
+    getId() {
+        return this.#id;
+    }
+
+    getTitle() {
+        return this.#title;
+    }
+
+    getImagePath() {
+        return this.#imagePath;
     }
 
     getShortText() {
-        return this.shortDescription;
+        return this.#shortDescription;
     }
 
     getDetails() {
-        return this.longDescription;
+        return this.#longDescription;
+    }
+
+    getType() {
+        return this.#type;
     }
 }
